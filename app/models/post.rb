a9498aa5e0ceb3b11c928a1c26e287376193435b
hidekazu <hide.14110924@gmail.com>
2022-06-02 12:20:27 +0900

    タイムゾーンの設定とscssの修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
 
-  validates :content, presence: true
+  validates :content, presence: true, length: { maximum:65535}
 
   def save_tags(tags)
 
