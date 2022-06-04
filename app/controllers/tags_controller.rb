5b90f53869a7fa4e0c5308e7d404492e9b61a3e3
hidekazu <hide.14110924@gmail.com>
2022-06-04 11:06:53 +0900

    タグに紐づいたユーザーの投稿を取得

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -7,6 +7,8 @@ class TagsController < ApplicationController
     # @tag = Tag.find(params[:id])
     user = User.find_by(id: current_user.id)
     @tag = user.tags.find(params[:id])
+    @tag.posts = @tag.posts.where(user_id: current_user.id).includes(:user).order("created_at DESC")
+    # タグに紐づいた現在のユーザーのみの投稿
   end
 
   def destroy
