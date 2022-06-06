8e2c1354f4e4205adb393f7455f6aeb9e37c43ee
hidekazu <hide.14110924@gmail.com>
2022-06-06 15:06:36 +0900

    管理者用のユーザー一覧の作成

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
     get 'login', to: 'user_sessions#new'
     post 'login', to: 'user_sessions#create'
     delete 'logout', to: 'user_sessions#destroy'
+    resources :users, only: [:index, :show, :edit, :update, :destroy]
   end
 end
  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
