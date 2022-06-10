ac4de020b7f5e22d7439c9416a14a987b680ddc1
hidekazu <hide.14110924@gmail.com>
2022-06-10 18:57:47 +0900

    定期実行の実装
    メールの自動送信の挙動を確認

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -1,8 +1,7 @@
 class PostMailer < ApplicationMailer
   def report_summary
-	  @created_post_count = Post.created.count
 
-		@post_created_at_yesterday = Post.created_at_yesterday
+	  @created_post_count = Post.count
 
     mail(to: 'admin@admin.com', subject: '投稿の集計結果')
   end
